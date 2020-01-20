df9c74118fb3c74f50a6315e7c2c4df71e0ec81e
Robus <github@robus.info>
2020-01-20 14:28:24 -0600

    feat: closest stops

---
+++ parsers/stops.parser.go
@@ -0,0 +1,37 @@
+package parsers
+
+import (
+	"GoCtaApi/api"
+	"bufio"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func GetStopCoordinates() []api.Stop {
+	csvFile, _ := os.Open("./data/stops.txt")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	var stops []api.Stop
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		if line[6] != "0" {
+			continue
+		}
+		lat, _ := strconv.ParseFloat(line[4], 64)
+		lon, _ := strconv.ParseFloat(line[5], 64)
+		stops = append(stops, api.Stop{
+			StopID:     line[0],
+			CommonName: line[2],
+			Lat:        lat,
+			Lon:        lon,
+		})
+	}
+	return stops
+}
