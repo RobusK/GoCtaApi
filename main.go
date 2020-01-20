479409020d687912b1d61acd252efe133b69e44c
Robus <github@robus.info>
2020-01-20 14:14:29 -0600

    refactor: graphql server

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
-	"github.com/friendsofgo/graphiql"
+	"GoCtaApi/api"
+	"GoCtaApi/services"
+	"github.com/graphql-go/handler"
 	"net/http"
+	"os"
 )
 
-func main() {
-	graphiqlHandler, err := graphiql.NewGraphiqlHandler("/graphql")
-	schema := gqlSchema()
-	if err != nil {
-		panic(err)
-	}
+var (
+	client            = api.NewAPIClient(getAPIKey())
+	directionService  = services.NewDirectionsService(client)
+	routeService      = services.NewRoutesService(client)
+	stopService       = services.NewStopsService(client)
+)
 
-	http.Handle("/graphql", gqlHandler(&schema))
-	http.Handle("/graphiql", graphiqlHandler)
+func getAPIKey() string {
+	if len(CtaAPIKey) == 0 {
+		return os.Getenv("CTA_KEY")
+	}
+	return CtaAPIKey
+}
+
+func main() {
+	schema := gqlSchema()
+
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+
+	http.Handle("/graphql", disableCors(h))
 	http.ListenAndServe(serverHostname+":"+serverPort, nil)
+}
+
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
